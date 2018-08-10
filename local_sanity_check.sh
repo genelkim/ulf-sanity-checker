188ee395658bf0a7245d9eeb8db8e3e42e17d535
Gene Kim <gkim21@cs.rochester.edu>
2018-08-09 21:43:12 -0400

    Made sanity_check.sh and sanity-check.cl work for AWS EC2.  Made a local version of sanity_check.sh so we don't need to edit every time we want to use it locally.

--- local_sanity_check.sh
+++ local_sanity_check.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-
-CHECKER_DIR="../ulf_sanity_checker"
-
-CURDIR=$(pwd)
-
 TEMPFILE="temp.preprocessed"
-
-cd $CHECKER_DIR
 
 python preprocessor.py ${1} ${TEMPFILE}
 ./sanity-check.cl ${TEMPFILE} 
 rm ${TEMPFILE}
-
-cd $CURDIR
 
